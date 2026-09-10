c855a87f2f3fb7ad51006292bc0e1af306fec2d4
ayoubsuk08-jpg <ayoubsuk08@gmail.com>
2026-09-10 01:21:48 +0100

    Update SESSION_ID default value in config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "FI9AGQrQ#HTUNrFiPoy8zXbMwjuo_lkt1v-_NOaEAxkwBOWCLQto",
+SESSION_ID: process.env.SESSION_ID || "5cki2KbY#-xIJpSbn-I3-dQwPK2HV10St3Y3kZ6NIuVEPe7DUvEI",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
