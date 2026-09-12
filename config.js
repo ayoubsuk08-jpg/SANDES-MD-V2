52950949e5ce5d81270a6072d0f8721aaa16c0c3
ayoubsuk08-jpg <ayoubsuk08@gmail.com>
2026-09-12 13:49:56 +0100

    Update SESSION_ID in config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "5cki2KbY#-xIJpSbn-I3-dQwPK2HV10St3Y3kZ6NIuVEPe7DUvEI",
+SESSION_ID: process.env.SESSION_ID || "gNt0QJ6A#qPx8jOSiy3DScS3eLfC4_VJQZkYU9s8NceJ8C81sRcc",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
