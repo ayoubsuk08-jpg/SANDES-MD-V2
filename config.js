e2356ef356fd1ac0f00c4c8fc359212625b6ef9f
ayoubsuk08-jpg <ayoubsuk08@gmail.com>
2026-09-12 14:01:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "gNt0QJ6A#qPx8jOSiy3DScS3eLfC4_VJQZkYU9s8NceJ8C81sRcc",
+SESSION_ID: process.env.SESSION_ID || "BIcRmZAQ#SZWyjToiVmuFhjlkawTmRydF8h4JT-GR5Dp-I0T8DfA",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://upld.zone.id/uploads/d4i0x5iq/sandes-md-v2.webp",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 };
